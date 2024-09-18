e9fefcaea995fd8a5f46f4380099ab7e840b3cd7
AyelmerCorzoB <corzobarbosaa@gmail.com>
2024-09-18 14:22:22 -0500

    feat: :sparkles: Correcion de trasabilidad y desorden de commits

--- componentes/titulo.js
+++ componentes/titulo.js
@@ -1,5 +1,5 @@
 const titulo = document.createElement("h1");
-titulo.textContent = "Tareas";
+titulo.textContent = "Tareas Diarias";
 
 const subtitulo = document.createElement("h2");
 subtitulo.textContent = "Tareas por hacer";
